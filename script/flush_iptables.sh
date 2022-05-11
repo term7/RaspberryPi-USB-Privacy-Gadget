30470591f4f879db8e0e77a7dd1ec07ee1c0e6ba
term7 <53258255+term7@users.noreply.github.com>
2022-05-11 18:24:39 +0000

    Update flush_iptables.sh

--- script/flush_iptables.sh
+++ script/flush_iptables.sh
@@ -14,7 +14,7 @@ iptables -X
 # Flush all counters too #
 iptables -Z
 
-# Flush and delete all nat and mangle #
+# Uncomment to flush and delete all nat and mangle #
 #iptables -t nat -F
 #iptables -t nat -X
 #iptables -t mangle -F
